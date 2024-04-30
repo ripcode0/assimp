662049148db6cd176ce283b9ec5d36bd69e18592
FatalError <ripcode0@gmail.com>
2024-05-01 07:50:44 +0900

    updated

--- prebuilt/msvc2017/x64/lib/cmake/assimp-5.0/assimpTargets-debug.cmake
+++ prebuilt/msvc2017/x64/lib/cmake/assimp-5.0/assimpTargets-debug.cmake
@@ -86,7 +86,7 @@ endif()
 # Commands beyond this point should not need to know the version.
 set(CMAKE_IMPORT_FILE_VERSION)
 
-get_filename_component(ASSIMP_ROOT_DIR "C:/@ripcode/assimp/installed/VS2017/x64" REALPATH)
+get_filename_component(ASSIMP_ROOT_DIR "C:/@ripcode/assimp/prebuilt/msvc2017/x64" REALPATH)
 set( ASSIMP_CXX_FLAGS ) # dynamically linked library
 set( ASSIMP_LINK_FLAGS "" )
 set( ASSIMP_LIBRARY_DIRS "${ASSIMP_ROOT_DIR}/lib")
